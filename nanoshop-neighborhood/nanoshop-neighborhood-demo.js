f9aa904dfda9fbf5105a1f9e3028c80b4e7791c2
jb0001994 <jb0001994@aol.com>
2016-03-10 03:41:01 -0800

    added primatives

--- nanoshop-neighborhood/nanoshop-neighborhood-demo.js
+++ nanoshop-neighborhood/nanoshop-neighborhood-demo.js
@@ -126,7 +126,7 @@
             NanoshopNeighborhood.applyFilter(
                 renderingContext,
                 renderingContext.getImageData(0, 0, canvas.width, canvas.height),
-                NanoshopNeighborhood.threshold
+                NanoshopNeighborhood.darkener
                 //NanoshopNeighborhood.averager // Convenience comment for easy switching.
             ),
             0, 0

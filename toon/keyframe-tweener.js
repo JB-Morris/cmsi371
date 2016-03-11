453124ef88bffff9145cb4c573d5d45d1c805323
J.B. Morris <jb0001994@aol.com>
2016-03-10 19:47:56 -0800

    fixing git merge error

--- toon/keyframe-tweener.js
+++ toon/keyframe-tweener.js
@@ -161,11 +161,17 @@
                     (distance / 2) * percentComplete * percentComplete + start :
                     (-distance / 2) * ((percentComplete - 1) * (percentComplete - 3) - 1) + start;
         },
-
+        
         quinticEaseOut: function (currentTime, start, distance, duration) {
             var percentComplete = currentTime / duration - 1;
             return distance * ((percentComplete) * percentComplete * percentComplete * percentComplete * percentComplete + 1) + start;
         },
+
+        // quinticEaseIn: function (currentTime, start, distance, duration) {
+        //     var percentComplete = currentTime / duration;
+        //     return distance * 
+        // },
+
 
         initialize: initializeAnimation
     };

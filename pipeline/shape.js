daeea69025ac4bebb5f7ecaefd10233ce6416f1d
jb0001994 <jb0001994@aol.com>
2016-03-28 22:52:10 -0700

    initial commit and beginings of custom "shape" method

---
+++ pipeline/shape.js
@@ -0,0 +1,120 @@
+var Shape = function (shapeParameters) {
+		if (!shapeParameters.shape) {
+			this.x = shapeParameters.x || 0,
+			this.y = shapeParameters.y || 0,
+			this.z = shapeParameters.z || 0,
+			this.vertices = shapeParameters.vertices || [],
+			this.indices = shapeParameters.indices || [],
+			this.mode = shapeParameters.mode || gl.LINES,
+			this.color = shapeParameters.color || [0.0, 0.0, 0.0],
+			this.children = shapeParameters.children || []
+		}else if (shapeParameters.shape == "icosahedron") {
+			
+		}
+		
+}
+
+
+/*
+ * This module defines/generates vertex arrays for certain predefined shapes.
+ * The "shapes" are returned as indexed vertices, with utility functions for
+ * converting these into "raw" coordinate arrays.
+ */
+var Shapes = function (shapeParameters) {
+
+
+
+    /*
+     * Returns the vertices for a small icosahedron.
+     */
+    icosahedron: function () {
+        // These variables are actually "constants" for icosahedron coordinates.
+        var X = 0.525731112119133606;
+        var Z = 0.850650808352039932;
+
+        return {
+            vertices: [
+                [ -X, 0.0, Z ],
+                [ X, 0.0, Z ],
+                [ -X, 0.0, -Z ],
+                [ X, 0.0, -Z ],
+                [ 0.0, Z, X ],
+                [ 0.0, Z, -X ],
+                [ 0.0, -Z, X ],
+                [ 0.0, -Z, -X ],
+                [ Z, X, 0.0 ],
+                [ -Z, X, 0.0 ],
+                [ Z, -X, 0.0 ],
+                [ -Z, -X, 0.0 ]
+            ],
+
+            indices: [
+                [ 1, 4, 0 ],
+                [ 4, 9, 0 ],
+                [ 4, 5, 9 ],
+                [ 8, 5, 4 ],
+                [ 1, 8, 4 ],
+                [ 1, 10, 8 ],
+                [ 10, 3, 8 ],
+                [ 8, 3, 5 ],
+                [ 3, 2, 5 ],
+                [ 3, 7, 2 ],
+                [ 3, 10, 7 ],
+                [ 10, 6, 7 ],
+                [ 6, 11, 7 ],
+                [ 6, 0, 11 ],
+                [ 6, 1, 0 ],
+                [ 10, 1, 6 ],
+                [ 11, 0, 9 ],
+                [ 2, 11, 9 ],
+                [ 5, 2, 9 ],
+                [ 11, 2, 7 ]
+            ]
+        };
+    },
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as triangles.
+     */
+    toRawTriangleArray: function (indexedVertices) {
+        var result = [];
+
+        for (var i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (var j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    },
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as line segments.
+     */
+    toRawLineArray: function (indexedVertices) {
+        var result = [];
+
+        for (var i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (var j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ],
+
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][(j + 1) % maxj]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    }
+
+};
